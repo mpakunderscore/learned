dbc2f35a79e580cd1d2552cd2e28a01e763f6ee2
mpakunderscore <mpakunderscore@gmail.com>
2020-03-05 22:33:58 -0600

    Info update

--- web/js/info.js
+++ web/js/info.js
@@ -1,14 +1,15 @@
 function initServiceInfo() {
 
     contentList.innerHTML = '' +
-        '<div>Last update: 04.03.2020</div>' +
-        '<div>Version: 0.3.4</div>' +
+        '<div>Last update: 05.03.2020</div>' +
+        '<div>Version: 0.3.5</div>' +
         // '<div>Mobile stub</div>' +
         // '<div>Border design</div>' +
-        '<div>Close branch by x fixed somehow</div>' +
+        // '<div>Close branch by x fixed somehow</div>' +
         // '<div>Gray categories only from current node, test</div>' +
-        '<div class="info">Personal graph, alpha 4</div>' +
-        '<div>Where is colors</div>' +
+        '<div>Mine links ui</div>' +
+        '<div class="info">Personal graph under development</div>' +
+        '<div>Where is design</div>' +
         '';
 
     // TODO card design
