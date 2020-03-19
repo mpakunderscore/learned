6378e20c1f131e9e8ce8d3cc8d749389ae40f37d
mpakunderscore <mpakunderscore@gmail.com>
2020-03-19 00:19:21 -0600

    Demo update

--- web/js/router.js
+++ web/js/router.js
@@ -4,9 +4,9 @@ let setPath = (path) => {
 
 if (window.location.href.endsWith('/demo/')) {
     setPath('/demo');
-    let demo = {id: 'Demo'}
-    clearGraph(demo);
     get('/demo/graph').then(response => {
+        let demo = {id: 'Demo'}
+        clearGraph(demo);
         if (response.graph['Y Combinator (company)']) {
             renderCustomGraph(response.graph, demo, 'Y Combinator (company)');
             initGraph();
@@ -17,9 +17,9 @@ if (window.location.href.endsWith('/demo/')) {
 if (window.location.href.endsWith('/coronavirus/')) {
     setPath('/coronavirus');
     clickMainCircle('Wash your hands!');
-    let coronavirus = {id: 'Coronavirus'};
-    clearGraph(coronavirus);
     get('/coronavirus/graph').then(response => {
+        let coronavirus = {id: 'Coronavirus'};
+        clearGraph(coronavirus);
         renderCustomGraph(response.graph, coronavirus, '2019–20 coronavirus pandemic');
         initGraph();
     });
