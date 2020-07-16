6915a0c4065f7bc2680bc288f2491e001c94de88
mpakunderscore <mpakunderscore@gmail.com>
2020-07-16 17:32:02 -0500

    Sources scheduler and api prod test 2

--- server/scheduler.js
+++ server/scheduler.js
@@ -10,8 +10,8 @@ exports.getStatus = () => {
     return exports;
 }
 
-// let timeout = 5 * 60 * 1000; // 5 min
-let timeout = 60 * 1000; // 1 min
+let timeout = 5 * 60 * 1000; // 5 min
+// let timeout = 60 * 1000; // 1 min
 // exports.timeout = 5 * 1000; // 5 sec
 // let timeout = 1 * 1000; // 1 sec
 
