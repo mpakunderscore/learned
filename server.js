01defe5e5d33ec74ddcb77ff2284653c2d0201d6
mpakunderscore <mpakunderscore@gmail.com>
2020-08-10 11:31:03 -0500

    Server fix

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.use('/coronavirus', express.static(__dirname + '/web'));
 // app.use('/chrome/learned.space.zip', express.static(__dirname + '/learned.space.zip'));
 
 api.init(app);
-scheduler.init()
+// scheduler.init()
 
 
