59bf2b65969a8ebfb40a315f18aa22cd5a07a83b
mpakunderscore <mpakunderscore@gmail.com>
2020-09-03 18:24:22 +0300

    Ups

--- web/main.js
+++ web/main.js
@@ -1,9 +1,7 @@
 let mainMessage = "" +
-    "Well. A lot of shit happened to me lately. My only option to keep developing is to combine this with my regular work. But to be honest, the founder's skills are poorly suited for part-time employment. Like you have the whole set to make a product, and not to file an assembly. I even have a job, but the depth of the model works by 70 percent (instead of 150 needed for such a process).\n" +
+    "Well. My only option to keep developing is to combine it this with regular work.\n" +
     "\n<br/><br/>" +
-    "Okay. This is all nagging, and now what to do. This list will be the easiest to comprehend.\n" +
-    "\n<br/><br/>" +
-    "The whole development stopped at the fact that the heroku unit is limited to 250MB of memory, or 512 as the ceiling. But in any case, my store began to grow and the crawlers clogged it very quickly, and the database itself.\n" +
+    "I'm stopped at the fact that the heroku unit is limited to 256MB of memory, or 512 as the ceiling. But in any case, my store began to grow and the crawlers clogged it very quickly, and the database itself.\n" +
     "\n<br/><br/>" +
     "It is necessary to optimize the work with memory from several sides, and I need the storage itself to quickly build a graph, queries to the database are too slow and I will not pull the graph database right now.\n" +
     "\n<br/><br/>" +
