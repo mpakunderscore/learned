d01a666d2707fee1c5b1352dc276ee80c3bfd51d
mpakunderscore <mpakunderscore@gmail.com>
2021-01-20 00:29:02 +0300

    Ups update

--- web/main.js
+++ web/main.js
@@ -1,12 +1,1 @@
-let mainMessage = "" +
-    "Well. My only option to keep developing is to combine it this with regular work.\n" +
-    "\n<br/><br/>" +
-    "I'm stopped at the fact that the heroku unit is limited to 256MB of memory, or 512 as the ceiling. But in any case, my store began to grow and the crawlers clogged it very quickly, and the database itself.\n" +
-    "\n<br/><br/>" +
-    "It is necessary to optimize the work with memory from several sides, and I need the storage itself to quickly build a graph, queries to the database are too slow and I will not pull the graph database right now.\n" +
-    "\n<br/><br/>" +
-    "After that, work begins with normal tasks, such as optimizing the work of crawlers for heavy links or bugs with the plugin.\n" +
-    "\n<br/><br/>" +
-    "And the most important thing. PDF with a description.\n" +
-    "\n<br/><br/>" +
-    "OK. Go."
+let mainMessage = "<a href='https://telegram.me/LearnedFoxBot'>@LearnedFoxBot</a>"
